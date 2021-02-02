8829953f4f5b0bb3243144f296cc7d6bb775b303
youngtommypickles <71870812+youngtommypickles@users.noreply.github.com>
2021-02-02 13:34:56 +0300

    Add files via upload

---
+++ os_lab4/child.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define MEMORY_NAME "file"
+#define MEMORY_SIZE 4096
+#define N 256
+
+typedef struct {
+    size_t size;
+    int sum[N];
+}Data;
+
+void func(FILE *input,Data *data) {
+    char c;
+	int a, b, res, flag;
+    a = 0; res =0; flag = 0;
+    data->size = 0;
+	for(;;) {
+        c = fgetc(input);
+
+        if (c >= '0' && c <= '9') {
+            b = atoi(&c);
+            a = a*10 + b;
+		}else if (c == '-') {
+			flag = 1;
+		}else if(c == ' ') {
+			if(flag){
+                res-=a;
+                flag = 0;
+            }else{
+                res+=a;
+            }
+            a = 0;
+		}else if(c == '\n') {
+			if(flag){
+                res-=a;
+                flag = 0;
+            }else{
+                res+=a;
+            }
+            data->sum[data->size++] = res;
+            res = 0;
+            a = 0;
+		}else if(c == EOF){
+            return;
+        }
+	}
+}
+
+int main(int argc, char *argv[]) {
+    int fd = shm_open(MEMORY_NAME, O_EXCL | O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if(fd < 0){
+        printf("Error sh_open\n");
+        return -2;
+    }
+    if(ftruncate(fd, MEMORY_SIZE)){
+        printf("Error with length of file\n");
+        return -3;
+    }
+
+    Data* data = mmap(NULL, MEMORY_SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
+    if(data == MAP_FAILED){
+        printf("Error map\n");
+        return -4;
+    }
+    FILE* input = fopen(argv[0], "r");
+    if(input ==  NULL){
+        printf("Error open file\n");
+        return -5;
+    }
+    func(input, data);
+    fclose(input);
+    munmap(data, MEMORY_SIZE);
+    close(fd);
+    return 0;
+}
---
+++ os_lab4/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define MEMORY_NAME "file"
+#define MEMORY_SIZE 4096
+#define N 256
+
+typedef struct {
+    size_t size;
+    int sum[N];
+}Data;
+
+int main(int argc, char *argv[]){
+    int status;
+    char name[N];
+    printf("Enter file name: ");
+    scanf("%s", name);
+    char* arg[] = {name, NULL}; 
+
+    pid_t id = fork();
+ 
+
+    if (id == 0) {
+        execv("./child", arg);
+    }else if(id > 0){
+        if(waitpid(id, &status, 0) == -1){
+            printf("Error wait child process\n");
+            return -1;
+        }    
+        if (WEXITSTATUS(status) != 0) {
+            return -2;
+        }
+        int fd = shm_open(MEMORY_NAME, O_RDONLY, S_IRUSR | S_IWUSR);
+        if(fd == -1){
+            printf("Error open shared memory file\n");
+            return -2;
+        }
+
+        Data *data = mmap(NULL, MEMORY_SIZE, PROT_READ, MAP_SHARED, fd, 0);
+        if(data == MAP_FAILED){
+            printf("Error map\n");
+            return -4;
+        }
+
+        for (int i = 0; i < data->size; i++) {
+            printf("%d Result from child: %d\n", i + 1, data->sum[i]);
+        }
+
+        munmap(data, MEMORY_SIZE);
+        shm_unlink(MEMORY_NAME);
+        close(fd);
+    } else{
+        printf("Error fork\n");
+        return -5;
+    }
+    return 0;
+}
